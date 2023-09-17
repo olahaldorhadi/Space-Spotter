bf84763c9e4557ea64278a787aca1efcdcb2aa8a
Hanna Lunne <hanlun0804@outlook.com>
2023-09-17 23:05:49 +0200

    Add list format for rooms

--- src/components/RoomManager.tsx
+++ src/components/RoomManager.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import { RoomObject, RoomManagerProps } from './RoomObject'
+import {  RoomManagerProps } from './RoomObject'
+import './RoomManager.css'
 
 function RoomManager({ rooms }: RoomManagerProps): React.ReactNode {
     return (
-        <div>
-            {rooms.map(RoomObject => (
-                <div key={RoomObject.key}>
-                    <h2>{RoomObject.name}</h2>
-                    <p>Size: {RoomObject.size}</p>
-                    <p>Bookable: {RoomObject.bookable}</p>
-                    <p>Type: {RoomObject.type}</p>
-                    <p>Building: {RoomObject.buildingname}</p>
-                    <p>Area: {RoomObject.areaname}</p>
+        <div className='RoomManager--EntireList'>
+            {rooms.map(room => (
+                // <div key={RoomObject.key}>
+                //     <h2>{RoomObject.name}</h2>
+                //     <p>Size: {RoomObject.size}</p>
+                //     <p>Bookable: {RoomObject.bookable}</p>
+                //     <p>Type: {RoomObject.type}</p>
+                //     <p>Building: {RoomObject.buildingname}</p>
+                //     <p>Area: {RoomObject.areaname}</p>
+                // </div>
+                <div className='RoomManager--ListContainer'>
+                    <div className="RoomManager--ListItem" key={room.key}>
+                        <p><b>{room.name}</b> Bygg: {room.buildingname} - Størrelse: {room.size} plasser</p>
+                    </div>
                 </div>
+
             ))}
         </div>
     );
